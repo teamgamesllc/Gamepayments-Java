6c7832e9d14c677016ee31e553fb99f7754a5a56
Nelson Sanchez <nelson@teamgames.io>
2020-05-24 14:04:20 -0400

    Fixes to checkout

--- src/com/teamgames/gamepayments/checkout/Checkout.java
+++ src/com/teamgames/gamepayments/checkout/Checkout.java
@@ -1,12 +1,14 @@
 package com.teamgames.gamepayments.checkout;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.google.gson.Gson;
 
 import com.teamgames.gamepayments.Configurations;
 import com.teamgames.gamepayments.ProductResponse;
+import com.teamgames.gamepayments.cart.CartItem;
 import com.teamgames.request.Connection;
 
 public class Checkout {
@@ -14,8 +16,10 @@ public class Checkout {
 	public boolean completedCheckout;
 	public String redirect;
 
-	public static ProductResponse fetch(String apiKey) throws Exception {
+	public static ProductResponse fetch(String apiKey, String username, List<CartItem> cartItems) throws Exception {
 		Map<String, Object> params = new LinkedHashMap<>();
+		params.put("username", username);
+		params.put("cartItems", cartItems);
 
 		final String ADDRESS = Configurations.isLocal ? Configurations.LOCAL_ADDRESS
 				: Configurations.GAMEPAYMENTS_ADDRESS;
